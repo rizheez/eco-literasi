8937aa54e567f4e6115d9baeea82ad673c888e63
rizheez <rezamuhammad980@gmail.com>
2026-06-27 20:12:25 +0800

    feat: add Capacitor mobile wrapper and platforms (android, ios)

---
+++ android/app/src/main/java/com/ecodayak/kids/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ecodayak.kids;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ecodayak.kids',
+  appName: 'ECO-DAYAK Kids',
+  webDir: 'dist'
+};
+
+export default config;
